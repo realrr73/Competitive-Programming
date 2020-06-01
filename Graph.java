bf7337c02d4590e591d5a00798a29aab164dce9b
realrr73 <66222689+realrr73@users.noreply.github.com>
2020-06-01 02:38:06 -0400

    Added Graph Class for Unweighted Graphs

---
+++ Graph.java
@@ -0,0 +1,82 @@
+import java.util.*;
+
+public class Graph {
+		int V;
+		LinkedList<Integer> adj[];
+		boolean [] visited;
+		
+		public Graph(int vertices) {
+			V = vertices;
+			this.V = vertices;
+			visited = new boolean[V];
+			adj = new LinkedList[V];
+			for(int i = 0; i<V; i++) {
+				adj[i] = new LinkedList<>();
+			}		
+		}
+		
+		void addEdge(int u, int v) { 
+			adj[u].add(v); 
+			adj[v].add(u); 
+		} 
+		
+		
+		void dfs(int u) { 
+	        visited[u] = true; 
+	  
+	        Iterator<Integer> i = adj[u].listIterator(); 
+	        
+	        while (i.hasNext()) { 
+	            int v = i.next(); 
+	            if (!visited[v]) {
+	            	dfs(v); 
+	            }
+	        } 
+	    } 
+		
+		void dfs2(int u) { 
+	        Stack<Integer> stack = new Stack<>(); 
+	          
+	        stack.push(u); 
+	          
+	        while(stack.empty() == false) { 
+	            u = stack.peek(); 
+	            stack.pop(); 
+	             
+	            if(visited[u] == false) { 
+	                visited[u] = true; 
+	            } 
+	              
+	            Iterator<Integer> itr = adj[u].iterator(); 
+	              
+	            while (itr.hasNext())  { 
+	                int v = itr.next(); 
+	                if(!visited[v]) {
+	                    stack.push(v); 
+	                }
+	            } 
+	              
+	        } 
+	    } 
+		
+		void bfs(int u) { 	
+		  	LinkedList<Integer> queue = new LinkedList<Integer>(); 
+		  	
+		  	visited[u] = true; 
+		  	
+		    queue.add(u); 
+		    
+		    while (queue.size() != 0) { 
+		    	u = queue.poll(); 	  
+		        Iterator<Integer> i = adj[u].listIterator(); 
+		        	while (i.hasNext())  { 
+		        		int v = i.next(); 
+		                if (!visited[v]) { 
+		                    visited[v] = true; 
+		                    queue.add(v); 
+		                } 
+		            } 
+		    } 
+		}
+
+}
